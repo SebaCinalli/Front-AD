f99fd538de4c514b134d319ae7701d67f2cb27c8
nanosalvu11 <salvuccinahuel@gmail.com>
2025-10-04 13:05:45 -0300

    modificando vite.config.ts para agregar el host permitido de render

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['front-ad-1.onrender.com']
+  }
 })
+
