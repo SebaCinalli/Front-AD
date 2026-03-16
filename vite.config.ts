13e12b6fcf23bf0e76185ac8d470fce6b255dd3a
nanosalvu11 <salvuccinahuel@gmail.com>
2026-03-16 13:49:42 -0300

    modificar package.json para solucionar error Error: listen EACCES: permission denied PORT

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['https://front-ad-2.onrender.com']
+    allowedHosts: ['front-ad-2.onrender.com']
   }
 })
 
