91a03e5699bc50869441dc0f6fdddcf3848d3750
nanosalvu11 <salvuccinahuel@gmail.com>
2026-03-16 13:13:38 -0300

    cambiando preview en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['front-ad-1.onrender.com']
+    allowedHosts: ['https://front-ad-2.onrender.com']
   }
 })
 
